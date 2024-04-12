b759d7a7478b949db6be01a30fb2a403e4f4ab90
bjack617 <bjack617@gmail.com>
2024-04-12 14:19:58 +0800

    feat: refactor array manipulation functions for performance improvements
    - Add a new file `find-second-max-number.js`
    - Implement a function `findSecondMaxNumber` to find the second max number in an integer array

---
+++ find-second-max-number.js
@@ -0,0 +1,22 @@
+// algorithms
+// Find second max number in integer array
+
+const inputs = [1, 5, 7, 3, 9, 2, 4, 6, 8, 0];
+
+const findSecondMaxNumber = (arr) => {
+  let max = arr[0];
+  let secondMax = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] > secondMax) {
+      secondMax = arr[i];
+    }
+  }
+
+  return secondMax;
+}
+
+console.log(findSecondMaxNumber(inputs)); // 8
